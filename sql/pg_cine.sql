b21ab08c1360b20b0d8f2aee575af306cabcd7a9
Matthias Colin <matthias.colin@gmail.com>
2020-09-23 13:26:33 +0200

    correct sql to add view + index

--- sql/pg_cine.sql
+++ sql/pg_cine.sql
@@ -1,11 +1,23 @@
 SET CLIENT_ENCODING = 'UTF-8';
 
+DROP schema vmovies1970 cascade;
+DROP schema vmovies1980 cascade;
+DROP schema vmovies1990 cascade;
+DROP schema vmovies2000 cascade;
+DROP schema vmovies2010 cascade;
+DROP schema vmovies2020 cascade;
 DROP SCHEMA movie cascade;
 DROP USER if exists movie;
 
 create user movie LOGIN password 'password';
 
 create schema movie authorization movie;
+create schema vmovies1970 authorization movie;
+create schema vmovies1980 authorization movie;
+create schema vmovies1990 authorization movie;
+create schema vmovies2000 authorization movie;
+create schema vmovies2010 authorization movie;
+create schema vmovies2020 authorization movie;
 
 set role movie;
 
@@ -15,3 +27,7 @@ set role movie;
 \i cine_data_stars.sql;
 \i cine_data_movies.sql;
 \i cine_data_play.sql;
+-- views
+\i pg_cine_views.sql
+-- indexes
+\i pg_cine_indexes.sql
---
+++ sql/pg_cine_indexes.sql
@@ -0,0 +1,2 @@
+create index idx_star_name on stars(name);
+create index idx_movie_year on movies(year);
